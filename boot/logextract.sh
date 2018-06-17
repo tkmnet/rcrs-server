1cf30c0f9082058a6c452fba908c746ed3111eb8
Shunki Takami <tkmnet@users.noreply.github.com>
2018-06-17 20:39:04 +0900

    Update logextract.sh

--- boot/logextract.sh
+++ boot/logextract.sh
@@ -5,7 +5,7 @@ if [ ! $# -eq 2 ] ; then
   exit 1
 fi
 
-. functions.sh
+. ./functions.sh
 
 makeClasspath $BASEDIR/jars $BASEDIR/lib
 java -Xmx1024m -cp $CP -Dlog4j.log.dir=logs rescuecore2.log.LogExtractor -c config/logviewer.cfg $1 $2
